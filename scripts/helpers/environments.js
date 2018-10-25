97bfecbde7bbe5c16f9f84ded85dcf1b4583aed0
Jorge Izquierdo <izqui97@gmail.com>
2018-10-25 14:20:24 +0200

    New structure for deployments with automated scripts + remove old deployment files (#8)
    * Move old deployment details to deprecated dir
    
    * Add version decompressor script
    
    * Remove old dependencies
    
    * Add husky hooks
    
    * WIP: save script fetch version and save IPFS archive
    
    * Remove mistakely commited dir
    
    * Implement save script
    
    * Delete deprecated files, update readme
    
    * Fix .yml file extension
    
    * Recover deprecated files
    
    * Address review comments

---
+++ scripts/helpers/environments.js
@@ -0,0 +1,22 @@
+module.exports = {
+  'local': {
+    ens: '0x5f6f7e8cc7346a11ca2def8f827b7a0b612c56a1',
+    node: 'http://localhost:8545'
+  },
+  'mainnet': {
+    ens: '0x314159265dd8dbb310642f98f50c066173c1259b',
+    node: 'https://mainnet.infura.io',
+  },
+  'rinkeby-old': {
+    ens: '0xfbae32d1cde62858bc45f51efc8cc4fa1415447e',
+    node: 'https://rinkeby.infura.io',
+  },
+  'rinkeby': {
+    ens: '',
+    node: 'https://rinkeby.infura.io',
+  },
+  'staging': {
+    ens: '0xfe03625ea880a8cba336f9b5ad6e15b0a3b5a939',
+    node: 'https://rinkeby.infura.io',
+  },
+}
